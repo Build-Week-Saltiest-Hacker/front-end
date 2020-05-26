4b5089f60f3f5571183f57e7e7dcfb68bea10e16
unknown <dethgrenade@gmail.com>
2020-05-26 09:43:38 -0700

    created useLocalStorage hook

--- react-app/src/utils/axiosWithAuth.js
+++ react-app/src/utils/axiosWithAuth.js
@@ -1,8 +1,10 @@
 import axios from 'axios'
 
 export const axiosWithAuth = () => {
+    //Get the token from local storage
     const token = localStorage.getItem('token')
 
+    //Create a header with the token authorization
     return axios.create({
         headers: {
             Authorization: token
