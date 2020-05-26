fb8855aa07ea4d410ad6e8d44d727c475f63a928
Francisco Barrios <shighetari@gmail.com>
2020-05-26 12:16:25 -0400

    Francisco-Barrios

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -4,6 +4,16 @@ function App() {
   return (
     <div className="App">
 
+      <h1> </h1>
+      <li> 
+    <Link to='/login'>Home</Link>
+  
+    <Link to='/pizza'>Order</Link>
+
+      </li>
+
+
+
     </div>
   )
 }
