6c96cd89f864021a499f4c078bb511ad14ca7d71
unknown <dethgrenade@gmail.com>
2020-05-26 11:35:03 -0700

    Redux implimentation started. Store and initial state setup.

--- react-app/src/index.js
+++ react-app/src/index.js
@@ -1,12 +1,31 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
+//Components
 import App from './App'
 
+/***************************************************************/
+/***********************     REDUX    **************************/
+/***************************************************************/
+//Redux Dependencies
+import { createStore, applyMiddleware } from 'redux'
+import { Provider } from 'react-redux'
+
+//Middleware
+import { thunk } from 'redux-thunk'
+
+//Reducers
+import rootReducer from './store/reducers'
+
+//Redux store
+const store = createStore(rootReducer, applyMiddleware(thunk))
+
+/***************************************************************/
 
 ReactDOM.render(
-
-  <App />
+  <Provider store={store}>
+    <App />
+  </Provider>
   ,
   document.getElementById('root')
 )
---
+++ react-app/src/store/reducers/appReducer.js
@@ -0,0 +1,15 @@
+export const initialState = {
+    token: '',
+    commentList: [],
+    savedComments: [],
+    userInfo: null,
+    isFetching: false
+}
+
+export const appReducer = (state = initialState, action) => {
+    switch (action.type) {
+
+        default:
+            return state
+    }
+}
