5f3f2b22ecd3cb943c40fd47a9bd2864fb1490b1
Francisco Barrios <shighetari@gmail.com>
2020-05-28 17:59:34 -0400

    push

--- react-app/src/components/UserDashboard.js
+++ react-app/src/components/UserDashboard.js
@@ -3,7 +3,7 @@ import { Link, useParams } from 'react-router-dom'
 import { axiosWithAuth } from '../utils/axiosWithAuth'
 import { useLocalStorage } from '../hooks/useLocalStorage'
 
-import CommentCard from './CommentCard'
+import CommentCard from './commentCard'
 
 //Redux
 import { connect } from 'react-redux'
